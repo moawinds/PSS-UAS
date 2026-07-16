45dfa8d9f98348b39cac5c7f9c47ddcdc8fd67f0
alwi <mohammadalwi@MacBook-Air-Alwi.local>
2026-07-17 03:18:02 +0700

    Add mock Google login endpoint to Ninja API

--- PROJECT UAS PSS/code/core/api/routes.py
+++ PROJECT UAS PSS/code/core/api/routes.py
@@ -44,3 +44,51 @@ def get_courses(
         qs = qs.filter(teacher__username__icontains=filters.teacher)
 
     return qs
+
+
+from pydantic import BaseModel
+from django.contrib.auth.models import User
+from ninja.errors import HttpError
+from ninja_simple_jwt.jwt.token_operations import get_access_token_for_user, get_refresh_token_for_user
+
+class GoogleLoginSchema(BaseModel):
+    email: str
+    first_name: str
+    last_name: str
+
+@router.post("/auth/google-login")
+def google_login(request, data: GoogleLoginSchema):
+    if not data.email or "@" not in data.email:
+        raise HttpError(400, "Email tidak valid")
+    
+    username = data.email.split("@")[0]
+    
+    user, created = User.objects.get_or_create(
+        email=data.email,
+        defaults={
+            'username': username,
+            'first_name': data.first_name,
+            'last_name': data.last_name,
+            'is_active': True
+        }
+    )
+    
+    if not created:
+        user.first_name = data.first_name
+        user.last_name = data.last_name
+        user.save()
+        
+    access_token, _ = get_access_token_for_user(user)
+    refresh_token, _ = get_refresh_token_for_user(user)
+    
+    return {
+        "access": access_token,
+        "refresh": refresh_token,
+        "user": {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name
+        }
+    }
